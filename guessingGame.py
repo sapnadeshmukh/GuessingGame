fd6069a52c3a2649b49f7429a39b1ffbdc476d5c
sapnadeshmukh <45514938+sapnadeshmukh@users.noreply.github.com>
2021-04-12 16:12:23 +0530

    Update guessingGame.py

--- guessingGame.py
+++ guessingGame.py
@@ -9,7 +9,7 @@ def listOfNumber(listofnum):
             list1.add(value)
         if len(list1)==5:
             break
-        i=i+1
+#         i=i+1
     return(list1)
 numbers=[0,1,2,3,4,5,6,7,8,9]
 num=listOfNumber(numbers)
